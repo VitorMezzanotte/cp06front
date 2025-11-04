4854a0e0edd4e7c296b7fc69e17f13ea5d42d4f3
Vitor <vitor.mconstante@gmail.com>
2025-11-03 21:46:06 -0300

    Tentativa de arrumar erro de estilização

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,5 @@
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('@tailwindcss/postcss'),
+  ],
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,9 @@
-export default {
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+});
